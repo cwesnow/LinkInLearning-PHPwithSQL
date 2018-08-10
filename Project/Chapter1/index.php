62283fa4c53e65995228ab434e20cd7c1384c9f8
Wesley Cox <cwesnow2000@yahoo.com>
2018-08-09 23:21:51 -0500

    Create index.php

---
+++ Project/Chapter1/index.php
@@ -0,0 +1,24 @@
+<? php
+  // Class Example - Demonstrates Require, Include, and being able to re-use code in PHP
+  ?>
+
+<? php
+  // General Functions
+  require_once('../../private/initialize.php'); ?>
+
+<? php
+  // Standard Header
+  include('../../private/shared/header.php'); ?>
+
+<div id="content">
+</div>
+
+<? php
+  // Standard Footer
+  include('../../private/shared/staff_footer.php'); ?>
+  
+<? php
+  // Example: using DEFINE to shorten path names - Useful for common Folders, Root structure, etc.
+  define("SHARED", 'localhost/shared');
+  include(SHARED . '/staff_footer.php');
+  ?>
